b963e68ab85a98ecf03042f33e07553302c2d7ef
Medusa-85 <lima.mariana012@gmail.com>
2023-08-15 13:33:09 -0300

    Estrutura base

---
+++ dice_game_simulation.py
@@ -0,0 +1,8 @@
+import random
+import numpy as np
+
+# Este código simula um jogo de lançamento de dados de seis lados. 
+# O objetivo é analisar a justiça do jogo, trazer possíveis resultados e fazer previsões sobre jogos futuros, 
+# utilizando para isso dados estatísticos relacionados a estes eventos.
+
+
